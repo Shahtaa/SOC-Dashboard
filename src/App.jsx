9513945fd3e4f591d70accb2cd754f4c93b9b01a
Alexei <>
2024-03-20 13:07:18 +0200

    css

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,27 @@
-// App.jsx
 import React from 'react';
 import PalomuuriComponent from './components/PalomuuriComponent';
-
 import ServerLogComponent from './components/ServerLogComponent';
 import ReititinComponent from './components/ReititinComponent';
 import TyoasemaComponent from './components/TyoasemaComponent';
+import './App.css'; // Import CSS file for styling
 
 const App = () => {
   return (
-    <div>
+    <div className="container">
       <h1>My SOC Dashboard</h1>
-      <div>
-        <PalomuuriComponent />
-      </div>
-      <div>
-        <ServerLogComponent />
-      </div>
-      <div>
-        <ReititinComponent />
-      </div>
-      <div>
-        <TyoasemaComponent />
+      <div className="grid-container">
+        <div className="grid-item">
+          <PalomuuriComponent />
+        </div>
+        <div className="grid-item">
+          <ServerLogComponent />
+        </div>
+        <div className="grid-item">
+          <ReititinComponent />
+        </div>
+        <div className="grid-item">
+          <TyoasemaComponent />
+        </div>
       </div>
     </div>
   );
